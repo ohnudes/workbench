8c1c31f6661a5ad86490fec7ae1de195b1a62e28
Nudes <nivohq@gmail.com>
2023-10-25 10:14:39 +0200

    Dev: pipe simulation finished.

--- pipex/4-pipesimulation.c
+++ pipex/4-pipesimulation.c
@@ -7,7 +7,8 @@
 
 int	main(int ac, char *av[], char *env[])
 {
-	char	*arg[] = {"bin/ls", NULL};
+	char	*ls[] = {"bin/ls", NULL};
+	char	*grep[] = {"bin/grep", "-B", "1", "3-forknpipe.c", NULL};
 	int		fd[2];
 	int		pid1; // cmd1
 	int		pid2; // cmd2
@@ -22,11 +23,25 @@ int	main(int ac, char *av[], char *env[])
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		execve("/bin/ls", arg, env);
+		execve("/bin/ls", ls, env);
 	}
 	else
+	{
 		pid2 = fork();
+		if (pid1 == -1)
+			return 3;
+	}
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execve("/bin/grep", grep, env);
+	}
+	close(fd[1]);
+	close(fd[0]);
 	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 
 	return (0);
 }
